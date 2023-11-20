11d3ed77d097f4337906e424845fff0e7836c483
Tega Rorobi <rorobitega@gmail.com>
2023-11-20 20:13:35 +0100

    Created a UsersViewSet

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import *
+
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
+
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+
+    "API ViewSet to list out, create, retrieve, update and delete users"
+
+    queryset = UserModel.objects.order_by('-id')
+    serializer_class = UserSerializer
