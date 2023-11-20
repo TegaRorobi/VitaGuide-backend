50db30f6219ec5e606cfa1960f4ad74efbd9bdf4
Tega Rorobi <rorobitega@gmail.com>
2023-11-20 17:13:55 +0100

    Created a custom User model, referenced it in the settings

--- user/models.py
+++ user/models.py
@@ -1,3 +1,30 @@
+
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+
+from django.utils.translation import gettext_lazy as _
+
+
+
+class User(AbstractUser):
+
+    username = models.CharField(
+        _("username"),
+        unique=True,
+        max_length=150,
+        validators=[UnicodeUsernameValidator()],
+        help_text=_("Not required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        error_messages={"unique": _("A user with that username already exists."),},
+        null=True, blank=True
+    )
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=150)
+    last_name = models.CharField(_("last name"), max_length=150, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
