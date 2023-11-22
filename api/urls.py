84f4ecd965d5ee56e95bcfe92cffb6531c3333f2
Tega Rorobi <rorobitega@gmail.com>
2023-11-22 19:32:07 +0100

    Registered the ChatView, made changes to the urlconf

--- api/urls.py
+++ api/urls.py
@@ -6,10 +6,16 @@ from .views import *
 
 
 urlpatterns = [
-    re_path('^users/?$', UsersViewSet.as_view(LIST_CREATE), name='users-list'),
-    re_path('^users/(?P<pk>\d+)/?$', UsersViewSet.as_view(RETRIEVE_UPDATE_DESTROY), name='user-detail'),
 
     re_path('^auth/login/?$', LoginView.as_view(), name='api-login'),
     re_path('^auth/login/refresh/?$', LoginRefreshView.as_view(), name='api-login-refresh'),
     re_path('^auth/logout/?$', LogoutView.as_view(), name='api-logout'),
+
+    re_path('^users/list/?$', UsersViewSet.as_view(LIST), name='users-list'),
+    re_path('^users/create/?$', UsersViewSet.as_view(CREATE), name='user-create'),
+    re_path('^user/(?P<pk>\d+)/retrieve/?$', UsersViewSet.as_view(RETRIEVE), name='user-retrieve'),
+    re_path('^user/(?P<pk>\d+)/update/?$', UsersViewSet.as_view(UPDATE_ANY), name='user-update'),
+    re_path('^user/(?P<pk>\d+)/delete/?$', UsersViewSet.as_view(DESTROY), name='user-delete'),
+    re_path('^user/(?P<pk>\d+)/chat/?$', ChatView.as_view(), name='user-chat'),
+
 ]
