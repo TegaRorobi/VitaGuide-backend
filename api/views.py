71ca6542d8accb417b3d64b110a5eaf7586f198e
Tega Rorobi <rorobitega@gmail.com>
2023-11-24 09:24:43 +0100

    Made changes to the ChatView

--- api/views.py
+++ api/views.py
@@ -3,20 +3,18 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
-
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenBlacklistView
 )
-
+from django.conf import settings
 from .serializers import *
-
+import requests
 from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
-import openai
-from django.conf import settings
+
 
 
 class UsersViewSet(ModelViewSet):
@@ -41,41 +39,29 @@ class LogoutView(TokenBlacklistView):
 
 class ChatView(GenericAPIView):
 
-    openai_model = 'gpt-3.5-turbo'
-    client = openai.OpenAI(api_key=settings.OPENAI_API_KEY)
+    model = 'gpt-3.5-turbo'
+    endpoint = 'https://api.openai.com/v1/chat/completions'
+    headers = {'Authorization': f'Bearer {settings.OPENAI_API_KEY}'}
 
     queryset = UserModel.objects.order_by('-id')
     serializer_class = ChatMessageSerializer
 
-    def get_completion_response(self, messages):
-        completion = self.client.chat.completions.create(
-            model=self.openai_model, 
-            messages=messages
-        )
-        return dict(completion.choices[0].message)
-
-        # return {
-        #     'role': 'assistant',
-        #     'content': 'I am ChatGPT!'
-        # }
+    def get_completion(self, messages):
+        data = dict(model=self.model, messages=messages)
+        res = requests.post(url=self.endpoint, headers=self.headers, json=data)
+        if hasattr(res, 'json'):
+            print(res.json())
+            return res['choices'][0]['message']
+        print(res)
+    
 
     def post(self, request, *args, **kwargs):
         user = self.get_object()
-
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            question = serializer.validated_data['question']
             messages = user.chat_logs['messages']
-            messages.append({'role':'user', 'content':question})
-
-            completion = self.get_completion_response(messages)
-            messages.append(completion)
-
-            user.chat_logs['messages'] = messages
+            messages.append({'role':'user', 'content':serializer.validated_data['question']})
+            messages.append(self.get_completion(messages))
             user.save()
-
-            return Response({
-                'response': completion['content']
-            }, status=status.HTTP_200_OK)
-
+            return Response(dict(response=messages[-1]['content']), status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
