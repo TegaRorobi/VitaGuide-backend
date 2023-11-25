3c668b456d5f6c5a7cd9138297dca9bc57f08d6e
Tega Rorobi <rorobitega@gmail.com>
2023-11-25 23:26:59 +0100

    Modified the save method on the ChatSession model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 UserModel = get_user_model()
@@ -11,6 +12,12 @@ class ChatSession(models.Model):
 
     created_at = models.DateTimeField(_('date created'), auto_now_add=True)
     updated_at = models.DateTimeField(_('last modified'), auto_now=True)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.content:
+            cnt = open((settings.BASE_DIR/'user/context.txt'), 'r').read().format(self.user.full_name)
+            self.content = {'messages': [{'role': 'system','content': cnt}]}
+        return super().save(*args, **kwargs)
     
     def __str__(self) -> str:
         return f"Chat session for {self.user.full_name}, created {self.created_at.strftime('%d/%m/%Y %H:%M:%S')}."
