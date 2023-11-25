16c66b6b271644eaab5a393a59626138af8c67da
Tega Rorobi <rorobitega@gmail.com>
2023-11-26 00:28:04 +0100

    Updated the urls, gathered all chat related logic into the ChatSessionViewSet

--- api/urls.py
+++ api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     re_path('^auth/login/refresh/?$', LoginRefreshView.as_view(), name='api-login-refresh'),
     re_path('^auth/logout/?$', LogoutView.as_view(), name='api-logout'),
 
-    re_path('^users/list/?$', UsersViewSet.as_view(LIST), name='users-list'),
+    re_path('^users/all/?$', UsersViewSet.as_view(LIST), name='users-list'),
     re_path('^users/create/?$', UsersViewSet.as_view(CREATE), name='user-create'),
 
     re_path('^user/me/retrieve/?$', UsersViewSet.as_view({'get':'my_details'}), name='user-retrieve'),
@@ -20,11 +20,9 @@ urlpatterns = [
     re_path('^user/(?P<pk>\d+)/delete/?$', UsersViewSet.as_view(DESTROY), name='user-delete'),
 
     re_path('^chat/sessions/all/?$', ChatSessionViewSet.as_view(LIST), name='chat-sessions-list'),
-    re_path('^chat/sessions/create/?$', ChatSessionViewSet.as_view(CREATE), name='chat-session-create'),
-    re_path('^chat/sessions/(?P<pk>\d+)/retrieve/?$', ChatSessionViewSet.as_view(RETRIEVE), name='chat-session-retrieve'),
-
-
-    re_path('^chat/user/(?P<pk>\d+)/?$', ChatViewSet.as_view({'post':'respond_to_user'}), name='user-chat'),
-    re_path('^chat/(?P<pk>\d+)/retrieve/?$', ChatViewSet.as_view({'get':'retrieve_chat_log'}), name='chat-retrieve'),
+    re_path('^chat/session/create/?$', ChatSessionViewSet.as_view(CREATE), name='chat-session-create'),
+    re_path('^chat/session/(?P<pk>\d+)/retrieve/?$', ChatSessionViewSet.as_view(RETRIEVE), name='chat-session-retrieve'),
+    re_path('^chat/session/(?P<pk>\d+)/interact/?$', ChatSessionViewSet.as_view({'post':'respond_to_user'}), name='user-chat'),
+    # re_path('^chat/(?P<pk>\d+)/retrieve/?$', ChatViewSet.as_view({'get':'retrieve_chat_log'}), name='chat-retrieve'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -46,39 +46,29 @@ class LogoutView(TokenBlacklistView):
 
 class ChatSessionViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
 
-    "API Viewset to create and retrieve chat sessions for the currently authenticated user"
+    "API Viewset to list out, create and retrieve the chat sessions belonging to the currently authenticated user"
+    
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return ChatSession.objects.filter(user=self.request.user).prefetch_related('user')
-    serializer_class = ChatSessionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_serializer_class(self):
+        if self.action == 'respond_to_user':
+            return ChatMessageSerializer
+        return ChatSessionSerializer
+    
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 
-class ChatViewSet(GenericViewSet):
-
     model = 'gpt-3.5-turbo'
     endpoint = 'https://api.openai.com/v1/chat/completions'
     request_headers = {'Authorization': f'Bearer {settings.OPENAI_API_KEY}'}
-
-    def get_queryset(self):
-        if self.action=='respond_to_user':
-            return UserModel.objects.order_by('-id')
-        return ChatSession.objects.all()
     
-    def get_serializer_class(self):
-        if self.action=='respond_to_user':
-            return ChatMessageSerializer
-        return ChatSessionSerializer
-
-
-    @action(detail=True)
-    def retrieve_chat_log(self, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+    
+    serializer_class = ChatMessageSerializer
 
 
     def get_completion(self, messages):
@@ -94,16 +84,16 @@ class ChatViewSet(GenericViewSet):
 
     @action(detail=True)
     def respond_to_user(self, request, *args, **kwargs):
-        chat_log = self.get_object().chat_log
+        session = self.get_object()
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            messages = chat_log.content['messages']
+            messages = session.content['messages']
             messages.append({'role':'user', 'content':serializer.validated_data['question']})
             status_code, completion = self.get_completion(messages)
             if status_code != 200:
                 return Response(completion or {'error':'Error processing request'}, status=status_code)
             messages.append(completion)
-            chat_log.content['messages'] = messages
-            chat_log.save()
+            session.content['messages'] = messages
+            session.save()
             return Response(dict(response=messages[-1]['content']), status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
