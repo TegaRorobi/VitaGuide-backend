fb59216d58b7328962ee3f61fc3a0511f3a928f1
Tega Rorobi <rorobitega@gmail.com>
2023-11-25 23:27:19 +0100

    Registered the ChatSession model in the admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ChatSession
 
-# Register your models here.
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    model = ChatSession
+    list_display = 'user', 'title', 'created_at', 'updated_at'
